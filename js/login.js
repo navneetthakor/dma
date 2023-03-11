667874931ed35ae360bb333108ce93fa75cb897d
Rohanshu-33 <banodharohanshu@gmail.com>
2023-03-12 01:27:24 +0530

    loginbuttonsetup

--- js/login.js
+++ js/login.js
@@ -2,3 +2,7 @@ function popsignuppage() {
     document.getElementById('sc').style.display = 'flex';
     document.getElementById('lc').style.display = 'none';
 }
+
+function poplogin(){
+    window.open("/html/login.html","_blank","width=500,height=400");
+}
