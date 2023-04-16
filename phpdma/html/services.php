19c5bdc0741293ba77022e688c46de92d4730bee
Rohanshu-33 <banodharohanshu@gmail.com>
2023-04-16 10:53:45 +0530

    addedphpdmafolder

---
+++ phpdma/html/services.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&family=Rokkitt:wght@300&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/dma/css/headerfooter.css">
+        <link rel="stylesheet" href="/dma/css/services.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>Document</title>
+
+</head>
+
+<body>
+    <!-- header of website -->
+    <header>
+        <!-- logo division  -->
+        <div id="logo">
+            <img src="/dma/images/cilogo.png" alt="CI MOGULS">
+        </div>
+
+        <!-- navigestion division  -->
+        <div id="navigation">
+            <nav>
+            <a href="/dma/html/index.php" class="navbar">Home</a>
+            <a href="/dma/html/services.php" class="navbar">Services</a>
+            <a href="/dma/html/plans.php" class="navbar">Plans</a>
+            <a href="/dma/html/contact.php" class="navbar">Contact Us</a>
+            </nav>
+        </div>
+        <div id="hbutton">
+            <input type="button" value="Login/Sign up" id="contactus" onclick="poplogin()">
+        </div>
+    </header>
+
+    <!-- header ends -->
+
+    <section class="sec21">
+        <div class="sem">
+            <div class="semimg">
+                <div class="blueback"></div>
+                <div class="cremeback"></div>
+                <img src="/dma/images/search-ads.jpeg" alt="search_ads_image" width="500vw" height="300vh">
+            </div>
+            <div class="seminfo">
+                <h2>Search Engine Marketing</h2>
+                <p>Search Engine Marketing (also known as SEM, PPC marketing) is a method of placing online
+                    advertisements on search engines based on similar user queries. We showcase your products by
+                    selecting combinations of appropriate keywords that out-results our end goal. The major 3
+                    benefits of using SEM are :</p>
+                <ul>
+                    <li>&#10003 Increase traffic to your website.</li>
+                    <li>&#10003 Increase the conversion rate.</li>
+                    <li>&#10003 Increase number of clicks.</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+    
+    <section class="sec22">
+        <div class="cm">
+            <div class="cminfo">
+                <h2>Content Marketing</h2>
+                <p>Content marketing is a marketing strategy used to attract, engage, and retain an audience by creating
+                    and sharing relevant articles, videos, podcasts, and other media. This approach establishes
+                    expertise, promotes brand awareness, and keeps your business top of mind when it's time to buy what
+                    you sell.</p>
+                <ul>
+                    <li>&#10003 Establish authority, trust, & brand awareness.</li>
+                    <li>&#10003 Increase website traffic.</li>
+                    <li>&#10003 Improves conversion rates.</li>
+                </ul>
+            </div>
+            <div class="cmimg">
+                <div class="blueback"></div>
+                <div class="cremeback"></div>
+                <img src="/dma/images/content-e1659954886320.jpeg" id="contimg" alt="search_ads_image" width="500vw" height="300px">
+            </div>
+        </div>
+    </section>
+
+
+
+    <section class="sec21">
+        <div class="sem">
+            <div class="semimg">
+                <div class="blueback"></div>
+                <div class="cremeback"></div>
+                <img src="/dma/images/seo-592740_640-pt5z0wz41ls3yndlzrk0g98bveowk1xcvoyftlk5uo.png" alt="search_ads_image" width="500vw" height="300px">
+            </div>
+            <div class="seminfo">
+                <h2>Search Engine Optimization</h2>
+                <p>Search Engine Optimization (SEO) is the process of improving the website which positively affects the ranking of the website. We achieve this by modifying your website's content according to SEO factors. Search Engine Optimization has key benefits such as :</p>
+                <ul>
+                    <li>&#10003 Increased traffic to your website
+                    </li>
+                    <li>&#10003 Improves your ranking.</li>
+                    <li>&#10003 Boosts your credibility.</li>
+                </ul>
+            </div>
+        </div>
+    </section>
+
+
+    <section class="sec22">
+        <div class="cm">
+            <div class="cminfo">
+                <h2>Influencer Marketing</h2>
+                <p>Influencer Marketing is a type of Social Media Marketing(SMM) that uses endorsements and product/service Mentions from influencers - Individuals who have a dedicated social following and are viewed as experts within their niche.</p>
+                <ul>
+                    <li>&#10003 Quickly improves Brand Awareness.</li>
+                    <li>&#10003 Effectively reaches your Target Audience(TA).</li>
+                    <li>&#10003 Building winning Partnerships.</li>
+                </ul>
+            </div>
+            <div class="cmimg">
+                <div class="blueback"></div>
+                <div class="cremeback"></div>
+                <img src="/dma/images/influencer-4492841_640.jpg" alt="search_ads_image" width="500vw" height="300px">
+            </div>
+        </div>
+    </section>
+
+
+    <!-- footer starts -->
+
+    <footer>
+        <div id="flogo">
+            <img src="/dma/images/cilogo.png" alt="">
+        </div>
+        <div id="fcontainer">
+            <div id="fcd1">
+                <h2>Navigate</h2>
+                <a href="">Home</a>
+                <a href="">Services</a>
+                <a href="">About us</a>
+                <a href="">Plans</a>
+            </div>
+            <div id="fcd2">
+                <h2>Get in Touch</h2>
+                <h3> <i class="material-icons">&#xe567; </i>&nbsp;  1509,Ramdevpura, near Khankuva, Bhalej road, Anand, Gujarat 388205</h3>
+                <h3><i class="material-icons">&#xe554; </i>&nbsp; bussiness@cimoguls.in</h3>
+                <h3><i class="material-icons">&#xe554; </i>&nbsp; customercare@cimoguls.in</h3>
+                <h3><i class="material-icons">&#xe551; </i>&nbsp; +91 8780905726</h3>
+            </div>
+            <div id="fcd3">
+                <h2>Locate Us</h2>
+                <div class="maps">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1180.1593083922057!2d72.92323211480819!3d22.55269380081014!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e4e74c03b7749%3A0xab364c66fd4834c!2sBirla%20Vishvakarma%20Mahavidyalaya%20(BVM)!5e0!3m2!1sen!2sin!4v1678513092528!5m2!1sen!2sin" width="150" height="150" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- footer ends -->
+    <script src="/dma/js/login.php"></script>
+</body>
+
+</html>
